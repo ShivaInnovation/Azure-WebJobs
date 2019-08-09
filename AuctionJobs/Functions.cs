b4b999434acbb322890981c8c95d281dcd6529a9
Shivappa <shivappakv@nousinfo.com>
2019-08-09 18:50:13 +0530

    first commit

---
+++ AuctionJobs/Functions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.Azure.WebJobs;
+
+namespace AuctionJobs
+{
+    public class Functions
+    {
+        internal static IHubProxy auctionHubClient;
+        public static async Task NotifyOfExpiringAuction(TextWriter log)
+        {
+            log.WriteLine("Checking for expired auctions.");
+
+            //get auctions expiring in the next two days
+            var targetTime = DateTime.Now.AddDays(2);
+
+            var ctx = new JobsAuctions.Models.AuctionDbContext();
+            var query = from auc in ctx.Auctions
+                        where auc.ClosingTime < targetTime && auc.ClosingTime > DateTime.Now
+                        select auc;
+
+            //for each item, notify the site through SignalR
+            foreach (var item in query)
+            {
+                log.WriteLine($"Notifying of expired auction {item.Title}");
+                try
+                {
+                    //call the signalr hub to notify clients
+                    await auctionHubClient.Invoke("expiringAuction", item.Title, item.ClosingTime);
+                }
+                catch (Exception ex)
+                {
+                    log.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ AuctionJobs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.Azure.WebJobs;
+
+namespace AuctionJobs
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        public static void Main()
+        {
+            while (true)
+            {
+                Functions.NotifyOfExpiringAuction(Console.Out);
+            }
+        }
+
+        private static async Task SetupSignalRClient()
+        {
+            var connection = new HubConnection("http://localhost/");
+            Functions.auctionHubClient = connection.CreateHubProxy("auctionHub");
+            try
+            {
+                await connection.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
+
+        }
+    }
+}
